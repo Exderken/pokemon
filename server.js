4c668d03766eba6f0a3ac243a00cf7b729c53c42
Franco Badan <francobadan@gmail.com>
2022-12-12 01:23:06 -0300

    parte2 - simular producto(dinamico) y rutas

--- server.js
+++ server.js
@@ -1,21 +1,45 @@
+const express = require("express");
+const app = express();
+const hbs = require("hbs");
+const port = 3000;
+// const pokemon = require("./data/pokedex.json")
+const productos = require("./data/products.json");
 
-const express = require('express')
-const app = express()
-const hbs = require("hbs")
-const port = 3000
-const pokemon = require("./data/pokedex.json")
+app.set("view engine", "hbs"); // indica que usaremos la plantilla "handlebars"
+hbs.registerPartials(__dirname + "/views/partials"); // indicamos el directorio hacia los parciales
 
-app.set('view engine', 'hbs'); // indica que usaremos la plantilla "handlebars"
-hbs.registerPartials(__dirname + '/views/partials'); // indicamos el directorio hacia los parciales
+app.use(express.static("public"));
 
-app.use(express.static("public"))
+app.get("/", (req, res) => {
+  res.render("index", {
+    titulo: "Mi pagina web",
+    producto: productos.products,
+  });
+});
 
-app.get('/', (req, res) => {
-res.render("index",{
-    pokemon:pokemon.pokemon
-  })
-})
+app.get("/como-comprar", (req, res) => {
+  res.render("como-comprar", {
+    titulo: "como-comprar"});
+});
+
+app.get("/contacto", (req, res) => {
+  res.render("contacto", {
+    titulo: "contacto"
+  });
+});
+
+app.get("/detalle-producto", (req, res) => {
+  res.render("detalle-producto", {
+    titulo: "detalle-producto"
+  });
+});
+
+app.get("/sobre-nosotros", (req, res) => {
+  res.render("sobre-nosotros", {
+    titulo: "sobre-nosotros"
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
